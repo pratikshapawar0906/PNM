fd74e66469b308261f1d5fb4b4d92092e0fbcda1
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-11-10 22:40:00 +0530

    First commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: "https://pnm-tgxe.onrender.com",
 });
 
 export const setAuthToken = (token) => {
