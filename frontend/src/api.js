4a44e71faa00fe3b31b07ea5a0f0ced7613d7df7
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-11-10 22:45:03 +0530

    api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pnm-tgxe.onrender.com",
+  baseURL: "https://pnm-tgxe.onrender.com/api",
 });
 
 export const setAuthToken = (token) => {
